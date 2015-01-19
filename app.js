e4cbb224554a0c73307f95920bf7cb632c3f55e7
Felix Rieseberg <felix@felixrieseberg.com>
2015-01-19 15:36:49 +0000

    Add example app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World from iojs ' + process.version + '!\n');
+}).listen(process.env.port);
